453e6c587dba3593c58a004771af0d818ce16fbd
Chris Kibble <chris@ckib.me>
2021-07-07 14:59:47 -0400

    Initial Release of MS Windows Releases JSON

---
+++ scripts/ms/mswin/GetWin10ReleaseIds.ps1
@@ -0,0 +1,23 @@
+﻿Clear-Host
+
+
+$msData = Invoke-WebRequest "https://winreleaseinfoprod.blob.core.windows.net/winreleaseinfoprod/en-US.html" -UseBasicParsing
+
+$allReleases = [RegEx]::New('(?msi)<span(?:[^>])class="triangle".*?>&#9660;<\/span>(?: |)<strong>Version (.*?) \(OS Build (\d{1,})\)<\/strong>').Matches($msData.RawContent)
+
+$releaseList = New-Object System.Collections.ArrayList
+
+$allReleases.ForEach{
+    $releaseList.add(
+        [PSCustomObject]@{
+            Version = $_.Groups[2].value
+            FullVersion = "10.0.$($_.Groups[2].Value)"
+            Build = $_.Groups[1].Value
+        }
+    ) | Out-Null
+}
+
+
+$releaseList | ConvertTo-Json | Out-File (Join-Path $outputFolder -ChildPath "releases.json")
+
+
