9e6d034da3ad4ec247875180b1bb53c8f324581d
Chris Kibble <chris@ckib.me>
2021-07-13 11:36:56 -0400

    Fixed m365releases variable name left over

--- scripts/ms/msother/mslocales.ps1
+++ scripts/ms/msother/mslocales.ps1
@@ -44,7 +44,7 @@ $outputData = [PSCustomObject]@{
 }
 
 $diffSinceLastUpdate = New-TimeSpan -Start $d4nData.DataForNerds.LastUpdatedUTC -End (Get-Date).ToUniversalTime()
-$allProperties = $m365Releases[0].psobject.Properties.Name
+$allProperties = $locales[0].psobject.Properties.Name
 
 If($diffSinceLastUpdate.TotalDays -ge 7 -or (Compare-Object $d4nData.Data $outputData.Data -Property $allProperties)) {
     $outputFolder = Resolve-Path (Join-Path $PSScriptRoot -ChildPath "../../../content/ms/msother")
