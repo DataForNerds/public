779325a72f46564427200a813d69cb5f13ef454c
Jacob Thornberry <altrhombus@outlook.com>
2023-01-25 12:06:20 -0600

    Windows 11 support
    Adds Windows 11 support, use aka.ms links

--- scripts/ms/mswin/releases.ps1
+++ scripts/ms/mswin/releases.ps1
@@ -1,26 +1,20 @@
-$msData = Invoke-WebRequest "https://winreleaseinfoprod.blob.core.windows.net/winreleaseinfoprod/en-US.html" -UseBasicParsing
-$d4nData = Invoke-WebRequest "https://raw.datafornerds.io/ms/mswin/releases.json" | Select -ExpandProperty Content | ConvertFrom-Json
-
-$allReleases = [RegEx]::New('(?msi)<span(?:[^>])class="triangle".*?>&#9660;<\/span>(?: |)<strong>Version (.*?) \(OS Build (\d{1,})\)<\/strong>').Matches($msData.RawContent)
+$msdata = @("https://learn.microsoft.com/en-us/windows/release-health/release-information",
+            "https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information")
+$d4nData = Invoke-WebRequest "https://raw.datafornerds.io/ms/mswin/releases.json" | Select-Object -ExpandProperty Content | ConvertFrom-Json
 
 $releaseList = New-Object System.Collections.ArrayList
 
-$allReleases.ForEach{
-    $releaseList.add(
-        [PSCustomObject]@{
-            Version = $_.Groups[2].value
-            FullVersion = "10.0.$($_.Groups[2].Value)"
-            Build = $_.Groups[1].Value
-        }
-    ) | Out-Null
-}
-
-## Fix issue where 19044 (21H2) still now showing up in the MS Blob
-if($releaseList.Version -notcontains "19044") {
-    $releaseList += [PSCustomObject]@{
-        Version = "19044"
-        FullVersion = "10.0.19044"
-        Build = "21H2"
+foreach ($sourceURL in $msdata) {
+    $source = Invoke-WebRequest $sourceURL -UseBasicParsing
+    $allReleases = [RegEx]::New('(?msi)<strong>Version (.*?) \(OS Build (\d{1,})\)<\/strong>').Matches($source.RawContent)
+    $allReleases.ForEach{
+        $releaseList.add(
+            [PSCustomObject]@{
+                Version = $_.Groups[2].value
+                FullVersion = "10.0.$($_.Groups[2].Value)"
+                Build = $_.Groups[1].Value
+            }
+        ) | Out-Null
     }
 }
 
@@ -29,7 +23,7 @@ $releaseList = $releaseList | Sort-Object Version | Select-Object Version,FullVe
 $outputData = [PSCustomObject]@{
     "DataForNerds"=[PSCustomObject]@{
         "LastUpdatedUTC" = (Get-Date).ToUniversalTime()
-        "SourceList" = @("https://docs.microsoft.com/en-us/windows/release-health/release-information","https://winreleaseinfoprod.blob.core.windows.net/winreleaseinfoprod/en-US.html")
+        "SourceList" = @("https://docs.microsoft.com/en-us/windows/release-health/release-information","https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information")
     }
     "Data" = $releaseList
 }
